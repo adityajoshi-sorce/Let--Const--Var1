97353bb5e2a56f34082877ee1749573a7955597e
adityajoshi-sorce <joshiaditya0727@gmail.com>
2023-07-08 14:10:06 +0530

    Fix this issue jbjasbjasbdjasbjd

---
+++ script.js
@@ -0,0 +1,12 @@
+let langauge = "English";
+langauge = "Hindi";
+console.log(langauge);
+
+const PI = 3.14;
+PI = 1.14;
+//console.log(PI);
+
+var a = 10;
+var b = 20;
+let c = a + b;
+console.log(PI);
